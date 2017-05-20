af8337d1fea56c4be334b1f685fb924b6ea04bb4
miguelramirez93 <darckzios@gmail.com>
2017-05-19 19:10:06 -0500

    se agrega consulta de categoria y mejoras a registro categoria

--- app/scripts/controllers/items/categoria_consulta.js
+++ app/scripts/controllers/items/categoria_consulta.js
@@ -8,10 +8,62 @@
  * Controller of the phantomclientApp
  */
 angular.module('phantomclientApp')
-  .controller('ItemsCategoriaConsultaCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ItemsCategoriaConsultaCtrl', function (phantomRequest) {
+    var self = this;
+    self.categoriaPadreSel = null;
+    self.filtroCat = "";
+    self.treeOptions = {
+    nodeChildren: "children",
+    dirSelectable: true,
+    injectClasses: {
+        ul: "a1",
+        li: "a2",
+        liSelected: "a7",
+        iExpanded: "a3",
+        iCollapsed: "a4",
+        iLeaf: "a5",
+        label: "a6",
+        labelSelected: "a8"
+    }
+  };
+    self.cargarArbolCategorias = function(){
+      phantomRequest.get('categoriacategoriarest/jerarquiacategorias','').then(function(response){
+        self.dataForTheTree = response.data;
+        console.log(response.data);
+      });
+    };
+
+    self.cargarArbolCategorias();
+
+    self.actualizarCategoria = function(){
+      phantomRequest.put('categoriarest/categorias',self.categoriaPadreSel.id,self.categoriaPadreSel).then(function(response){
+        if (response.data == true){
+          swal('','Se Actualizo la categoria.','success');
+        }else if (response.data.message != undefined) {
+          if(response.data.message[0] === "Error Number: 1062"){
+            swal('','La categoria con nombre '+self.categoria_a_registrar.nombre+' ya se encuentra registrada.', 'error');
+          }else{
+            swal('','error del servidor.', 'error');
+          }
+        }else{
+          swal('','error del servidor.', 'error');
+        }
+      });
+    };
+    self.eliminarCategoria = function(){
+      phantomRequest.delete('categoriarest/categorias',self.categoriaPadreSel.id).then(function(response){
+        if (response.data == true){
+          swal('','Se Elimino la categoria.','success');
+          self.cargarArbolCategorias();
+        }else if (response.data.message != undefined) {
+          if(response.data.message[0] === "Error Number: 1451"){
+            swal('','La categoria con nombre '+self.categoriaPadreSel.nombre+' esta siendo referenciada como hijo o padre de otra categoria.', 'error');
+          }else{
+            swal('','error del servidor.', 'error');
+          }
+        }else{
+          swal('','error del servidor.', 'error');
+        }
+      });
+    };
   });
--- app/scripts/controllers/items/categoria_registro.js
+++ app/scripts/controllers/items/categoria_registro.js
@@ -8,14 +8,14 @@
  * Controller of the phantomclientApp
  */
 angular.module('phantomclientApp')
-  .controller('ItemsCategoriaRegistroCtrl', function (phantomRequest) {
+  .controller('ItemsCategoriaRegistroCtrl', function(phantomRequest) {
     var self = this;
     self.categoriaPadreSel = null;
     self.filtroCat = "";
     self.treeOptions = {
-    nodeChildren: "children",
-    dirSelectable: true,
-    injectClasses: {
+      nodeChildren: "children",
+      dirSelectable: true,
+      injectClasses: {
         ul: "a1",
         li: "a2",
         liSelected: "a7",
@@ -24,48 +24,50 @@ angular.module('phantomclientApp')
         iLeaf: "a5",
         label: "a6",
         labelSelected: "a8"
-    }
-  };
-    self.cargarArbolCategorias = function(){
-      phantomRequest.get('categoriacategoriarest/jerarquiacategorias','').then(function(response){
+      }
+    };
+    self.cargarArbolCategorias = function() {
+      phantomRequest.get('categoriacategoriarest/jerarquiacategorias', '').then(function(response) {
         self.dataForTheTree = response.data;
       });
     };
 
-    self.limpiar = function(){
+    self.limpiar = function() {
       self.nombreCat = null;
-       self.descrpCat = null;
-       self.categoriaPadreSel = null;
+      self.descrpCat = null;
+      self.categoriaPadreSel = null;
     };
 
     self.cargarArbolCategorias();
 
-    self.RegistrarCategoria = function(){
+    self.RegistrarCategoria = function() {
       self.categoria_a_registrar = {};
-      if (self.nombreCat == null){
-        swal('','El nombre de la categoria es requerido.','error');
-      }else {
+      if (self.nombreCat == null) {
+        swal('', 'El nombre de la categoria es requerido.', 'error');
+      } else {
         self.categoria_a_registrar.nombre = self.nombreCat;
         self.categoria_a_registrar.descripcion = self.descrpCat;
         var url = 'categoriarest/categorias?';
-        if(self.categoriaPadreSel == undefined || self.categoriaPadreSel == null){
+        if (self.categoriaPadreSel == undefined || self.categoriaPadreSel == null) {
 
-        }else{
-          url = url + 'padre='+ self.categoriaPadreSel.id
+        } else {
+          url = url + 'padre=' + self.categoriaPadreSel.id
         }
-        phantomRequest.post(url,self.categoria_a_registrar).then(function(response){
+        phantomRequest.post(url, self.categoria_a_registrar).then(function(response) {
           console.log(response.data);
-          if (response.data == true){
-            swal('','Se registro la categoria.','success');
+          if (response.data == true) {
+            swal('', 'Se registro la categoria.', 'success');
             self.limpiar();
             self.cargarArbolCategorias();
-          }else if (response.data.message != undefined) {
-            if(response.data.message[0] === "Error Number: 1062"){
-              swal('','La categoria con nombre '+self.categoria_a_registrar.nombre+' ya se encuentra registrada.', 'error');
-            }else{
-              swal('','error del servidor.', 'error')
+          } else if (response.data.message != undefined) {
+            if (response.data.message[0] === "Error Number: 1062") {
+              swal('', 'La categoria con nombre ' + self.categoria_a_registrar.nombre + ' ya se encuentra registrada.', 'error');
+            } else {
+              swal('', 'error del servidor.', 'error')
             }
 
+          } else {
+            swal('', 'error del servidor.', 'error')
           }
         });
 
